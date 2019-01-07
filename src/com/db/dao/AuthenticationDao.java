0578b3097ecf1706a4a2740248dfbde1f8f159b6
satyam.kumar <satyamk328@gmail.com>
2019-01-07 15:49:13 +0530

    ssssss

--- src/com/db/dao/AuthenticationDao.java
+++ src/com/db/dao/AuthenticationDao.java
@@ -1,7 +1,9 @@
 package com.db.dao;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
@@ -11,16 +13,17 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.db.model.User;
+import com.db.utils.SecurityDigester;
 
 @Repository("userDetailsDao")
 public class AuthenticationDao {
 
 	private static final Logger log = LoggerFactory.getLogger(AuthenticationDao.class);
-	
+
 	@Autowired
 	private SessionFactory sessionFactory;
 
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public List<User> getUsers() {
 		log.info("call getUsers()");
 		return sessionFactory.getCurrentSession().createCriteria(User.class).list();
@@ -34,27 +37,58 @@ public class AuthenticationDao {
 
 	@Transactional
 	public boolean lockUser(String userName, boolean isLock, int attempt) {
-		sessionFactory.getCurrentSession().createQuery("update User set attempt =:attempt, islock =:lock where uid =:uid")
+		sessionFactory.getCurrentSession()
+				.createQuery("update User set attempt =:attempt, islock =:lock where uid =:uid")
 				.setParameter("attempt", attempt).setParameter("lock", isLock).setParameter("uid", userName)
 				.executeUpdate();
 		return true;
 	}
-	
-	@Transactional(readOnly=true)
+
+	@Transactional(readOnly = true)
 	public User forgotPassword(String email) {
-		 return (User) sessionFactory.getCurrentSession().createCriteria(User.class).add(Restrictions.eq("email", email).ignoreCase()).setFetchSize(1).setMaxResults(1);
+		return (User) sessionFactory.getCurrentSession().createCriteria(User.class)
+				.add(Restrictions.eq("email", email).ignoreCase()).setFetchSize(1).setMaxResults(1);
 	}
-	
+
 	@Transactional
 	public boolean changePassword(String email, String pass) {
 		sessionFactory.getCurrentSession().createQuery("update User set password =:password where userid =:uid")
-		.setParameter("password", pass).setParameter("uid", email)
-		.executeUpdate();
+				.setParameter("password", pass).setParameter("uid", email).executeUpdate();
 		return true;
 	}
-	
+
 	@Transactional
-	public User authUser(String email, String pass) {
-		 return (User) sessionFactory.getCurrentSession().createCriteria(User.class).add(Restrictions.eq("email", email).ignoreCase()).add(Restrictions.eq("password", pass).ignoreCase()).setFetchSize(1).setMaxResults(1);
+	public User authUser(String email, String pass) throws UnsupportedEncodingException {
+		User user = null;
+		if (validatePhoneNumber(email))
+			user = (User) sessionFactory.getCurrentSession().createCriteria(User.class)
+					.add(Restrictions.eq("phone", email).ignoreCase())
+					.add(Restrictions.eq("password", SecurityDigester.decrypt(pass)).ignoreCase()).setFetchSize(1)
+					.setMaxResults(1);
+		else
+			user = (User) sessionFactory.getCurrentSession().createCriteria(User.class)
+					.add(Restrictions.eq("email", email).ignoreCase())
+					.add(Restrictions.eq("password", SecurityDigester.decrypt(pass)).ignoreCase()).setFetchSize(1)
+					.setMaxResults(1);
+		return user;
 	}
+
+	private static boolean validatePhoneNumber(String phoneNo) {
+		// validate phone numbers of format "1234567890"
+		if (phoneNo.matches("\\d{10}"))
+			return true;
+		// validating phone number with -, . or spaces
+		else if (phoneNo.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}"))
+			return true;
+		// validating phone number with extension length from 3 to 5
+		else if (phoneNo.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}"))
+			return true;
+		// validating phone number where area code is in braces ()
+		else if (phoneNo.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}"))
+			return true;
+		// return false if nothing matches the input
+		else
+			return false;
+	}
+
 }
