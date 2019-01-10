f6528e882078e0cbbf5bb9b63da6c119c5959c40
satyam.kumar <satyamk328@gmail.com>
2019-01-10 23:51:26 +0530

    xxxxxxxxxxx

--- src/com/db/controller/AuthenticationController.java
+++ src/com/db/controller/AuthenticationController.java
@@ -32,10 +32,19 @@ public class AuthenticationController {
 	public ResponseEntity<RestResponse<Object>> registration(@RequestBody(required = true) User user) {
 		log.info("call registration {}", user);
 		RestStatus<String> status = new RestStatus<>(HttpStatus.OK.toString(), "User Registered Successfully");
-		User user1 = userServiceDetails.addUser(user);
-		if (user1 != null)
-			status = new RestStatus<>(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "User not Registered Successfully");
-		return new ResponseEntity<>(new RestResponse(user1, status), HttpStatus.OK);
+		if (userServiceDetails.getUserDetails(user.getEmail()) == null) {
+			status = new RestStatus<>(HttpStatus.CONFLICT.toString(),
+					"A user with this email address already exist into system!");
+		} else if (userServiceDetails.getUserDetails(user.getPhoneNumber()) == null) {
+			status = new RestStatus<>(HttpStatus.CONFLICT.toString(),
+					"A user with this phone number already exist into system!");
+		} else {
+			 user = userServiceDetails.addUser(user);
+			if (user != null)
+				status = new RestStatus<>(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+						"User not Registered Successfully");	
+		}
+		return new ResponseEntity<>(new RestResponse(user, status), HttpStatus.OK);
 	}
 
 	@PostMapping(value = "/serviceLoginAuth")
@@ -59,7 +68,8 @@ public class AuthenticationController {
 		RestStatus<String> status = new RestStatus<>(HttpStatus.OK.toString(), "Forgot password Successfully");
 		User user = userServiceDetails.getUserDetails(email);
 		if (user == null)
-			status = new RestStatus<>(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Invalid Email/password. Please enter valid email!");
+			status = new RestStatus<>(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+					"Invalid Email/password. Please enter valid email!");
 		return new ResponseEntity<>(new RestResponse(user, status), HttpStatus.OK);
 	}
 
@@ -68,8 +78,9 @@ public class AuthenticationController {
 			@RequestParam(name = "newPassword", required = true) String pass) throws UnsupportedEncodingException {
 		RestStatus<String> status = new RestStatus<>(HttpStatus.OK.toString(), "Forgot change Successfully");
 		int i = userServiceDetails.changePassword(uid, pass);
-		if(i == 0)
-			status = new RestStatus<>(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Currently this service is unavailable. We regret the inconvenience caused. Please try after some time.");
+		if (i == 0)
+			status = new RestStatus<>(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+					"Currently this service is unavailable. We regret the inconvenience caused. Please try after some time.");
 		return new ResponseEntity<>(new RestResponse(true, status), HttpStatus.OK);
 	}
 
--- src/com/db/dao/AuthenticationDao.java
+++ src/com/db/dao/AuthenticationDao.java
@@ -13,7 +13,6 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
