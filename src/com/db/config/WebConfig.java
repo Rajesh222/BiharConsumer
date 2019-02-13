9d81e23cf5d3be24855d8e3394c6da08c370cead
satyam.kumar <satyam.kumar@GDC-SatyamK-LPT.agreeya.local>
2019-02-13 11:27:25 +0530

    aaa

--- src/com/db/config/WebConfig.java
+++ src/com/db/config/WebConfig.java
@@ -19,7 +19,7 @@ import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
@@ -27,7 +27,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableTransactionManagement
 @EnableScheduling
 @PropertySource({"classpath:application.properties","classpath:sql.properties"})
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer  {
 
 	@Autowired
 	private Environment env;
