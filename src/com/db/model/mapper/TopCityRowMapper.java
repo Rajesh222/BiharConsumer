c2325bb07c8fa8ab79ab4aa124ab1511eac71286
satyam.kumar <satyam.kumar@GDC-SatyamK-LPT.agreeya.local>
2019-02-21 00:54:50 +0530

    aaa

--- src/com/db/model/mapper/TopCityRowMapper.java
+++ src/com/db/model/mapper/TopCityRowMapper.java
@@ -15,7 +15,7 @@ public class TopCityRowMapper implements RowMapper<TopCities> {
 	@Override
 	public TopCities mapRow(ResultSet rs, int rowNum) throws SQLException {
 		TopCities user = new TopCities();
-		user.setId(rs.getInt("refnumber"));
+		user.setId(rs.getInt("stopid"));
 		user.setDisplayName(rs.getString("displayname"));
 		user.setCityName(rs.getString("cityname"));
 		user.setStateName(rs.getString("statename"));
